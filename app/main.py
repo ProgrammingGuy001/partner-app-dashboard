d0829fe5c708185e4ec74960fc5b201105998be4
Ayush Mishra <ce4182022@niamt.ac.in>
2026-01-25 01:53:30 +0530

    trying new fix

--- app/main.py
+++ app/main.py
@@ -41,9 +41,16 @@ app.add_exception_handler(RateLimitExceeded, rate_limit_exceeded_handler)
 
 
 
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+    "http://localhost:5173",
+    "https://partner-app-dashboard-navy.vercel.app",
+]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
